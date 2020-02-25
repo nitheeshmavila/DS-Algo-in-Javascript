d4c2f099f3b8a719be924799ea64967643b10aef
nitheeshmavila <mail2nitheeshmavila@gmail.com>
2020-02-25 22:51:06 +0530

    bubble sort added

---
+++ sorting/bubbleSort.js
@@ -0,0 +1,22 @@
+// Bubble sort 
+// Time - O(N^2)
+// Space - O(N)
+
+function bubbleSort(array){
+	let swap = false;
+	for(let i=0; i<array.length; i++){
+		//let swap = false;
+		for(let j=0; j<array.length - i - 1; j++){
+			if(array[j] >= array[j+1]){
+				swap = true;
+				[array[j], array[j+1]] = [array[j+1], array[j]];
+			}
+		}
+		if(swap === false){
+			break;
+		}
+	}
+	return array;
+}
+
+console.log(bubbleSort([21,3,322,234,2,4,53,32]));
